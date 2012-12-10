370ba753d677c2835f659b717c10511b671ebf79
Travis Bybee <travis.bybee@gmail.com>
2012-12-10 00:45:59 -0700

    Moved filter logic to main screen (TodoTxtTouch.java). NFC Filter tag didn't work calling Filter.java. Filter.java is called with an intent that stores a lot of extras, all the currently existing priorities, tasks and contexts. A better way to call filter would be to use the TaskBag class within filter, removing a lot of intent data from TodoTxtTouch to Filter.

--- src/com/todotxt/todotxttouch/TodoTxtTouch.java
+++ src/com/todotxt/todotxttouch/TodoTxtTouch.java
@@ -272,6 +272,47 @@ OnSharedPreferenceChangeListener {
 	protected void onResume() {
 		super.onResume();
 		setFilteredTasks(true);
+
+		//Check to see if the activity was called with NFC NDEF.
+		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+			//Get NDEF message
+			NdefMessage[] message = getNdefMessages(getIntent());
+			//Get the information from the NDEF message.
+			byte[] payload = message[0].getRecords()[0].getPayload();
+			//Place info into a string.
+			String nfcMessage = new String(payload);
+			String delimiter = " ";
+			String separatedMessage[] = nfcMessage.split(delimiter);
+			
+			System.out.println(separatedMessage.toString());
+		}
+	}
+
+	private NdefMessage[] getNdefMessages(Intent intent) {
+		//Parse the intent
+		NdefMessage[] msgs = null;
+		String action = intent.getAction();
+		//If the Intent action is the NDEF intent filter action, parse the message.
+		if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)	|| NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+			//Get the messages from the NFC NDEF.
+			Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+			if (rawMsgs != null) {
+				msgs = new NdefMessage[rawMsgs.length];
+				for (int i = 0; i < rawMsgs.length; i++) {
+					msgs[i] = (NdefMessage) rawMsgs[i];
+				}
+			} else {
+				// Unknown tag type.
+				byte[] empty = new byte[] {};
+				NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+				NdefMessage msg = new NdefMessage(new NdefRecord[] {record});
+				msgs = new NdefMessage[] {msg};
+			}
+		} else {
+			Log.d(TAG, "Unknown intent.");
+			finish();
+		}
+		return msgs;
 	}
 
 	@Override
